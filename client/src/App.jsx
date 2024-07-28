001d2303d969feaa0cb20433e4dcd095a755f5e7
SarikaRamamoorthy <myothermailid001@gmail.com>
2024-07-28 07:37:36 +0530

    created routing and header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element = { <Home />} />
+        <Route path='/sign-up' element = { <SignUp />} />
+        <Route path='/sign-in' element = { <SignIn />} />
+        <Route path='/profile' element = { <Profile />} />
+        <Route path='/about' element = { <About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
