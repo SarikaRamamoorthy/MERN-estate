fd1409ea31115345440f90009b3a15d7a4181841
SarikaRamamoorthy <myothermailid001@gmail.com>
2024-09-13 20:57:16 +0530

    bug fix

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, '/client/dist')))
 
 
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.js'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.js'));
 })
 
 
